7a2e568ea10ffddd3561772b7212acee5157afa2
Ahmad-46 <mubbaranauman@gmail.com>
2025-07-03 09:04:42 -0700

    country code

---
+++ main2.py
@@ -0,0 +1,9 @@
+country_code = {'pakistan' : '0092',
+                 'Australia' : '0025', 
+                 'Bangledesh' : '0880'}
+
+print("country code for pakistan -")
+print(country_code.get('pakistan', 'Not found'))
+
+print("country code for japan -")
+print(country_code.get('japan', 'Not found'))
